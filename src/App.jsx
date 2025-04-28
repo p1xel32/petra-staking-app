198e2c4a4aaf6d411daba352939bb698e73d9b32
p1xel32 <118385076+p1xel32@users.noreply.github.com>
2025-04-28 17:57:20 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import ConnectWalletButton from './components/ConnectWalletButton';
+import ValidatorInfo from './components/ValidatorInfo';
+import StakeUnstakeControls from './components/StakeUnstakeControls';
+import './index.css';
+
+function App() {
+  const [account, setAccount] = useState(null);
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <h1 className="text-3xl font-bold mb-8">Petra Staking App</h1>
+      <ConnectWalletButton onConnect={setAccount} />
+      {account && (
+        <>
+          <ValidatorInfo />
+          <StakeUnstakeControls account={account} />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
