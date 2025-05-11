8b383027228a098860891f6bef9cdb9c671697d1
j.fogel <jjjf@jjjf.local>
2025-05-11 19:06:27 -0400

    Fix: trailingSlash_always_output_static

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -8,7 +8,9 @@ import react from "@astrojs/react"; // If you plan to use React components
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aptcore.one', // Your main domain
-  base: '/blog',             // The subpath for your blog
+  base: '/blog',               // The subpath for your blog
+  trailingSlash: 'always',     // important for paths
+  output: 'static',            // ← вот здесь нужна была запятая
   integrations: [
     mdx(),
     tailwind({
