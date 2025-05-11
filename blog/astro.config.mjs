cb53852b5f0871950e30369030c22994b25059de
j.fogel <jjjf@jjjf.local>
2025-05-11 19:34:01 -0400

    trailingSlash

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,5 +1,6 @@
 // blog/astro.config.mjs
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
@@ -8,11 +9,10 @@ import react from "@astrojs/react";
 export default defineConfig({
   site: 'https://aptcore.one',
   base: '/blog',
-  trailingSlash: 'always',
-  output: 'static',
-  adapter: vercel({ // Add the adapter configuration
-    webAnalytics: { enabled: true }, // Optional: if you use Vercel Analytics
-    // You can add other Vercel-specific options here if needed
+  trailingSlash: 'never',
+  output: 'server', // Важно для адаптера Vercel
+  adapter: vercel({
+    webAnalytics: { enabled: true }, // Опционально
   }),
   integrations: [
     mdx(),
