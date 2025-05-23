c070d3c30ee6a08d3617c2a2c64aa2a25a57ef86
j.fogel <jjjf@jjjf.local>
2025-05-23 12:28:11 -0400

    fix: alias antd/es/* to antd/lib/* to resolve avatar imports on Linux

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,44 @@
-// vite.config.js
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import path from 'path'; // Only if you use path for other aliases
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    preserveSymlinks: true,
+    alias: [
+      // всё, что идёт из antd/es/... будет браться из lib
+      {
+        find: /^antd\/es\/(.*)$/,
+        replacement: path.resolve(__dirname, 'node_modules/antd/lib/$1'),
+      },
+    ],
+  },
   build: {
     target: 'es2020',
     minify: 'esbuild',
     cssCodeSplit: true,
   },
-  resolve: {
-    preserveSymlinks: true,
-    // alias: { // This antd/es alias is likely not needed if optimizeDeps works
-    //   'antd/es': path.resolve(__dirname, 'node_modules/antd/es'),
-    // },
-  },
   optimizeDeps: {
     include: [
-      'antd/es/grid',          
-      'antd/es/layout',       
-      'antd/es/space',        
-      'antd/es/button',       
-      'antd/es/form',         
-      'antd/es/input',        
-      'antd/es/input-number', 
-      'antd/es/checkbox',     
-      'antd/es/tooltip',      
-      'antd/es/spin',         
-      'antd/es/alert',        
-      'antd/es/typography',   
-      'antd/es/divider',      
+      'antd/es/grid',
+      'antd/es/layout',
+      'antd/es/space',
+      'antd/es/button',
+      'antd/es/form',
+      'antd/es/input',
+      'antd/es/input-number',
+      'antd/es/checkbox',
+      'antd/es/tooltip',
+      'antd/es/spin',
+      'antd/es/alert',
+      'antd/es/typography',
+      'antd/es/divider',
       'antd/es/avatar',
-      'antd/es/avatar/group',       // Crucial for the "./avatar" error
-      // If you had a "./flex" error before, these might have helped:
-      // 'antd/es/flex', 
+      'antd/es/avatar/group',
     ],
+  },
+  ssr: {
+    noExternal: ['antd'],
   },
 });
