6bc7ba7d94ff4b196eae03dabcb2f14dfef04c9c
j.fogel <jjjf@jjjf.local>
2025-06-02 21:25:58 -0400

    debug: vike build

--- pages/+Page.jsx
+++
@@ -1,9 +0,0 @@
-// pages/index/+Page.jsx
-import React from 'react';
-import MainStakingPage from '../../src/pages/MainStakingPage/MainStakingPage';
-
-export default function Page() {
-  return (
-    <MainStakingPage />
-  );
-}
